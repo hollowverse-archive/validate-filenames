15f4f1905c94dcc013f41aed7d605139daaf5ab0
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-22 16:16:24 +0200

    Do not escape non ASCII characters in git ls-files
    The `-z` flag tells `git` to output the filenames in zero-terminated, non-escaped form.
    
    See https://stackoverflow.com/questions/5110904/is-git-ls-files-really-character-agnostic-if-not-what-is
    
    This should fix hollowverse/hollowverse#226

--- src/getFilenames.ts
+++ src/getFilenames.ts
@@ -2,11 +2,11 @@ import * as shelljs from 'shelljs';
 
 export function getFilenames() {
   // Let's only validate files managed by git
-  const { stdout } = shelljs.exec('git ls-files', { silent: true });
+  const { stdout } = shelljs.exec('git ls-files -z', { silent: true });
 
   if (typeof stdout === 'string') {
     // remove empty strings from the array
-    return stdout.split('\n').filter(file => file.length !== 0);
+    return stdout.split("\u0000").filter(file => file.length !== 0);
   }
 
   console.error('Unable to read git tree, is this a git repository?');
